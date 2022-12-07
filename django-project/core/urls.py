8a636ed81cea6757a88a77cecd5829affb30a50b
Kevin Washington <87237986+KevinWashington@users.noreply.github.com>
2022-12-07 17:19:46 -0300

    Update urls.py

--- django-project/core/urls.py
+++ django-project/core/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, entrar, registrar, salvar, deletar, search
+from .views import home, search, salvar, deletar, entrar, registrar
 #, update
 
 urlpatterns = [
-    path('', home,  name="search"),
+    path('', home, name="search"),
     path('login/', entrar, name="login"),
     path('salvar/', salvar, name="salvar"),
     path('registrar/', registrar, name="registrar"),
     path('deletar/<int:id>', deletar, name='deletar'),
-    path('search/<str:search>', search, name='search'),
-    # path('update/<int:id>', update, name='edit'),
-]
+    path('search/', search, name='search'), #<str:search>
+    
