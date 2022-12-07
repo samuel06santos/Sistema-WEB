11497d793360ac2a5604dbc02c6f9977a4f34fe7
Kevin Washington <87237986+KevinWashington@users.noreply.github.com>
2022-12-07 17:20:22 -0300

    Update models.py

--- django-project/core/models.py
+++ django-project/core/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
     user = models.CharField(max_length=100)
-
+    first_name = models.CharField(max_length=50, default = "")
+    last_name = models.CharField(max_length=50, default = "")
+    email = models.CharField(max_length=50, default = "")
+    senha = models.CharField(max_length=50, default = "")
     def __str__(self):
         return self.user
